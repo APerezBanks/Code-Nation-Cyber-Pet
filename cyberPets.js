5533ac731b2dd4423973b49139060bc7c732d447
Alejandro Perez Banks <alejandroperezbanks@MacBook-Pro-de-Alejandro.local>
2022-02-03 16:04:16 +0000

    new

--- cyberPets.js
+++ cyberPets.js
@@ -5,227 +5,78 @@
 // – if you don’t play, it gets bored, if you do, it’s happy, 
 // but gets thirsty, that kind of thing.
  
-// class Animal {
-//     constructor (name, hunger, thirst, play){
-//         this._name = name;
-//         this._hunger = hunger;
-//         this._thirst = thirst;
-//         this._play = play;
-//     }
-//     get name() {
-//                 return this._name;
-//             }
-//             get hunger(){
-//                 return this._hunger;
-//             }
-//             get thirst(){
-//                 return this._thirst;
-//             }
-//             eat() {
-//                 this._hunger--;
-//             }
-//             drink(){
-//                 this._thirst--;
-//             }
-//             playtime() {
-//         if (this_play > 50 ){
-//             console.log(`${this._name} is having a lot of fun `);
-//         } else {
-//         console.log(`ohhh nooo ${this._name} is very bored,try to play with him.`);
-//         }
-//     }
-// }
 
-// const dog = new Animal("Toffee",80,50, 67);
-// console.log(dog)
+//attemp number 3409532442-023 
 
-// class Animal {
-//     constructor(name) {
-//         this._name = name;
-//         this._hunger = 100;
-//         this._thirst = 100;
-//     }
-//     get name() {
-//         return this._name;
-//     }
-//     get hunger(){
-//         return this._hunger;
-//     }
-//     get thirst(){
-//         return this._thirst;
-//     }
-//     eat() {
-//         this._hunger--;
-//     }
-//     drink(){
-//         this._thirst--;
-//     }
-// }
+// user selects the kind of animal ? how ?????? we not covered that
 
-// class Bunny extends Animal {
+// just going to create  one object called animal 
 
-//     constructor(name, lovesCarrot, favFood){
-//     super(name);
-//     this._lovesCarrot = lovesCarrot; 
-//     this._favFood = favFood;
-//     }
-//     get lovesCarrots(){
-//         return this._lovesCarrot; }
-//     get favFood(){
-//         return this._favFood;
-// } }
+class Animal {
+    constructor (name, animalType, hunger, thirst, energy) {
+        this._name = name;
+        this._animalType = animalType;
+        this._hunger = hunger;
+        this._thirst = thirst;
+        this._fun = ["Sad","Bored","happy"];
+        this._energy = energy;
+    }
+    get name() {
+        return this._name;
+    }
+    get animaltype () {
+        return this.animalType;
+    }
+    get hunger () {
+        return this._hunger
+    }
+    get thirst () {
+        return this.thirst;
+    }
+    get fun () {
+        return this.energyFun()
+    }
+    get energy () {
+        return this._energy;
+    }
 
-// const rosie = new Bunny("Rosie",true,["basil", "rockets", "broccoli"]);
+    reduceHunger(amount) {
+        return this._hunger -= amount;
+    }
 
-
-// console.log(rosie)
-
-// class Rat extends Animal {
-
-//     constructor(name, radiation){
-//     super(name);
-//     this._radiationLevel = radiation; 
-//     this._littleRats =  0;
-//     }
-//     get radiation(){
-//         return this._radiantLevel; }
-//     get littleRats(){
-//         return this._littleRats;
-//     }
-//     increaseRat(){
-//         this.littleRats++;
-//     }
-
-// }
-
-// const splinter = new Rat("Splinter","max level",0) 
-
-// console.log(splinter)
-
-
-
-
-
-
-// class Enemy {
-//     constructor(name, health) {
-//         this._name = name;
-//         this._health = health;
-//         this._weapons = ["sword", "bow"];
-//     }
-//     get name() {
-//         return this._name;
-//     }
-//     get health() {
-//         return this._health;
-//     }
-//     get weapons() {
-//         return this._weapons;
-//     }
-//     reduceHealth(amount) {
-//         return this._health -= amount;
-//     }
-// }
-
-// const enemy1 = new Enemy("Jordan", 1000);
-// const enemy2 = new Enemy("Yusuf", 10);
-
-// console.log(enemy1.health);
-// console.log(enemy1.reduceHealth(100));
-// console.log(enemy2.health);
-// console.log(enemy1.reduceHealth(100));
-// console.log(enemy2.health);
-// console.log(enemy1.reduceHealth(100));
-// console.log(enemy2.health);
-// console.log(enemy2.reduceHealth(10));
-
-
-// class Human {
-//     constructor (name, energy, experience, money, social){
-//     this._name = name;
-//     this._energy = energy;
-//     this._experience = experience;
-//     this._money = money;
-//     this._social = social;
-// }
-//     get name() {
-//         return this._name;
-//     }
-//     get energy(){
-//         return this._energy;
-//     }
-//     get experience(){
-//         return this._experience;
-//     }
-//     get money() {
-//         return this._money;
-//     }
-
-//     get social(){
-//         return this._social;
-//     }
-
-// }
+    energyFun(){
+            if (this._energy >= 60)
+                    return this._fun[2]
+          } else if {
+                (this._energy <= 60)
+                    return this._fun[1]
+        } else if { 
+                (this._energy <= 30)
+                    return this._fun[0]
+    }
+        
+ }
     
-// const humanoide = new Human ("Robert", 50, 60, 23, 10)
-
-//  console.log(humanoide)
 
 
-//  class BabyBoomer extends Human {
-//     constructor()
+class Dog extends Animal {
+    constructor(name,animalType, hunger, thirst,fun, energy){
+        super(name,animalType, hunger, thirst,fun, energy);
+        this._treat = ["chiken","Doggy Snack","bone"];
+    }
+    get treat(){
+       return this._treat;
+     }
 
-//  }
+} 
 
+const dog = new Animal("Sammy", "dog", 100, )
+console.log(dog.reduceHunger(10));
+console.log(Animal);
 
-//  class GenX extends Human {}
-
-//  class Xennial extends Human {}
-
-//  class Millenial extends Human {}
-
-//  class Zgen extends Human {}
-
-
-// class Animal {
-//     constructor(){
-//         this._health = 1000;
-//     }
-//     action(){
-//         console.log("I did something");
-//     }
-//     get health(){
-//         return this._health;
-//     }
-// } 
-
-// class Bird extends Animal {
-//     constructor(){
-//         super();
-//         this._health = 1050;
-//         this._favouriteFood = ["Seeds"];
-//     }
-//     action(){
-//         console.log("I flapped my wings")
-//     }
-// } 
-
-// class Dog extends Animal {
-//     constructor(){
-//         super();
-//         this._favouriteFood = ["Dog food", "Postman's trousers"];
-//     }
-//     action(){
-//         super.action();
-//         console.log("I barked");
-//     }
-// } 
-
-// let bird = new Bird(); 
-// let dog = new Dog(); 
-
-// console.log(bird.health); // Bird's health will be 1050 
-// console.log(dog.health); // Dog's health will be 1000
-
-// bird.action(); // Bird's action() will result "I flapped my wings"
-// dog.action(); // Dog's action() will result "I did something" and "I barked"
+const sammy = new Dog ("d","doggy",12,23,"happy","10");
+console.log(fun)
+const sammy = new Dog ("d","doggy",12,23,"happy","50");
+console.log(fun)
+const sammy = new Dog ("d","doggy",12,23,"happy","70");
+console.log(fun)
